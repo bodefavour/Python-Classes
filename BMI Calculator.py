548a1d03574b49fd779c6dc80a2875697caa1203
Favour Bode <bodefavour@gmail.com>
2024-08-23 19:23:17 +0100

    feat: Add BMI Calculator and even/odd program

---
+++ BMI Calculator.py
@@ -0,0 +1,2 @@
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your height in kg: "))
---
+++ even and odd program.py
@@ -0,0 +1,8 @@
+number = int(input("What number do you want to check? "))
+
+result = int(number) % 2 
+
+if (result == 0):
+    print("The number is an even number")
+else:
+    print("The number is an even number")
