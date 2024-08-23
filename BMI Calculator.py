bcf93b62cd8e3c7cd71956f78776f643eaf83ef9
Favour Bode <bodefavour@gmail.com>
2024-08-23 19:28:20 +0100

    feat: Add BMI Calculator with classification

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,2 +1,15 @@
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your height in kg: "))
+
+BMI = weight /height **2
+
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}, your are underweignt")
+elif BMI < 25:
+    print(f"Your BMI is {BMI}, your are normal weight")
+elif BMI < 30:
+    print(f"Your BMI is {BMI}, your are overweight")
+elif BMI < 35:
+    print(f"Your BMI is {BMI}, your are obese")
+else:
+    print(f"Your BMI is {BMI}, your are clinically obese")
