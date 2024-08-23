0ff5367094d2633c4087fa7387da3c36a843fd5a
Favour Bode <bodefavour@gmail.com>
2024-08-23 19:31:51 +0100

    Fix rounding issue in BMI calculation

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,7 +1,7 @@
 height = float(input("Enter your height in m: "))
 weight = float(input("Enter your height in kg: "))
 
-BMI = weight /height **2
+BMI = round(weight /height **2)
 
 if BMI < 18.5:
     print(f"Your BMI is {BMI}, your are underweignt")
